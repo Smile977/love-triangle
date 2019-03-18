98505521f1e9566f44c4816cbb76ba2af5d23a58
Smile977 <9771919@mail.ru>
2019-03-18 10:25:25 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,27 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  a = preferences.map(x=>x-1);
+let triangles_count = 0;
+let visited = {}; 
+for(let i = 0; i<a.length; i++){
+  if(i in visited){
+    continue;
+  } 
+  let k = a[i];
+  let tmp_visited = {}; 
+tmp_visited[i] = 0;
+  while(!(tmp_visited.hasOwnProperty(k))){
+    tmp_visited[k] = Object.keys(tmp_visited).length;    
+    k = a[k];
+  }
+  if(Object.keys(tmp_visited).length == 3 && tmp_visited[k] === 0){ 
+    triangles_count++;
+    Object.keys(tmp_visited).forEach((x)=>{
+      visited[x] = true;
+    });
+  }
+}
+return triangles_count;
+
 };
